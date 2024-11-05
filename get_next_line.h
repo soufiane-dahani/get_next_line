7874c88fbd43a39a9e482857ed6abd38cb5a4841
Soufiane Dahani <sodahani@e1r10p11.1337.ma>
2024-11-05 13:09:17 +0100

    add files

--- get_next_line.h
+++ get_next_line.h
@@ -1,18 +1,25 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: sodahani <sodahani@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/11/05 10:46:39 by sodahani          #+#    #+#             */
-/*   Updated: 2024/11/05 11:20:50 by sodahani         ###   ########.fr       */
+/*   Created: 2024/11/05 10:46:53 by sodahani          #+#    #+#             */
+/*   Updated: 2024/11/05 11:22:36 by sodahani         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "get_next_line.h"
+#ifndef GET_NEXT_LINE
+# define GET_NEXT_LINE
+# include <stddef.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <stdio.h>
+# include <fcntl.h>
+#define BUFFER_SIZE 100
 
-char    *get_next_line(int fd)
-{
-    
-}
+
+
+
+#endif
